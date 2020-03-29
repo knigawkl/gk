34f5657130a0bcd5675b7c3f826e4b37a4403473
knigawkl <lukasz.knigawka@outlook.com>
2020-03-29 18:15:34 +0200

    refactor

--- helpers.py
+++ helpers.py
@@ -1,6 +1,12 @@
 import bios
+import argparse
 
 
 def read_cfg(cfg_path: str) -> dict:
-    config = bios.read(cfg_path)
-    return config
+    return bios.read(cfg_path)
+
+
+def get_parser() -> argparse.PARSER:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--conf_path')
+    return parser
--- virtual_camera.py
+++ virtual_camera.py
@@ -1,28 +1,26 @@
 import math
 import numpy
 from typing import List
-import argparse
 import tkinter
 
-from helpers import read_cfg
+from helpers import read_cfg, get_parser
 
 
 priority = lambda p: math.sqrt(sum([e**2 for e in numpy.mean(numpy.array(p), axis=0)]))
 
 
-def render(canvas: tkinter.Canvas, polygons: List[List[int]], outline_colour: str, distance: int,
-           screen_height: int, screen_width: int):
+def render(canvas: tkinter.Canvas, polygons: List[List[int]], outline: str, dist: int, height: int, width: int):
     canvas.delete(tkinter.ALL)
     polygons = sorted(polygons, key=priority, reverse=True)
     for polygon in polygons:
         points = []
         for point in polygon:
-            point_2d = project(point, distance, screen_height, screen_width)
+            point_2d = project(point, dist, height, width)
             points.append(point_2d)
-        canvas.create_line(points[0], points[1], fill=outline_colour)
-        canvas.create_line(points[1], points[2], fill=outline_colour)
-        canvas.create_line(points[2], points[3], fill=outline_colour)
-        canvas.create_line(points[3], points[0], fill=outline_colour)
+        canvas.create_line(points[0], points[1], fill=outline)
+        canvas.create_line(points[1], points[2], fill=outline)
+        canvas.create_line(points[2], points[3], fill=outline)
+        canvas.create_line(points[3], points[0], fill=outline)
     canvas.pack()
 
 
@@ -32,10 +30,7 @@ def project(point: List[int], distance: int, height: int, width: int):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--conf_path')
-    args = parser.parse_args()
-
+    args = get_parser().parse_args()
     cfg = read_cfg(args.conf_path)
     bg_colour = cfg["bg_colour"]
     outline_colour = cfg["outline_colour"]
